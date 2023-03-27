c5959549bf586cba6303d9a853f6c1ab9d4afa34
Dmitrii Kim <mitya_kim@icloud.com>
2023-03-26 17:55:16 -0700

    Initial Commit

---
+++ Combine_and_UIKit_example3/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Combine_and_UIKit_example3
+//
+//  Created by Mitya Kim on 3/26/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
